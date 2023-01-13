d574aee85a00f0ed89a66bf2e3aec525621146a8
amenpunk <ondasycircuitos@gmail.com>
2023-01-13 11:45:54 -0600

    fix

--- convert.py
+++ convert.py
@@ -1,2 +1,7 @@
-with open('RMYD1CLG.txt', 'r') as file: 
-    for linea in file: print('<xsd:element name="' + linea.strip() + '" type="xsd:string"/>')
+with open('./COPY/RMYD1CLG.cbl', 'r') as file: 
+    for linea in file: 
+        if "PIC" in linea :
+            posicion = linea.find("PIC")
+            temp = linea[0:posicion].strip() 
+            texto_sin_numeros = "".join(filter(lambda x: not x.isdigit(),temp))
+            print('<xsd:element name="' + texto_sin_numeros.strip() + '" type="xsd:string"/>')
